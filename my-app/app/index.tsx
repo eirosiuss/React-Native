849fac65dbdd56366b5d1a79524708a1ea68ee13
Kestutis Eirosius <eirosiuss@gmail.com>
2025-12-20 16:50:51 +0200

    add data util

--- my-app/app/index.tsx
+++ my-app/app/index.tsx
@@ -1,8 +1,31 @@
-import { Text } from "react-native";
-import React from "react";
+import { Text, View, TextInput, FlatList } from "react-native";
+import { useState } from "react";
+import { searchCountry } from "../app/utils/data";
 
-const Cat = () => {
-  return <Text>Hello, I am  your cat!</Text>
+export default function Cat() {
+  const [query, setQuery] = useState("");
+  const [results, setResults] = useState<any[]>([]);
+
+  return (
+    <View>
+      <TextInput
+        placeholder="Enter country name"
+        value={query}
+        onChangeText={async (text) => {
+          setQuery(text);
+          if (text.length > 2) {
+            const data = await searchCountry(text);
+            setResults(data);
+          }
+        }}
+        style={{ borderWidth: 1, padding: 8, margin: 8 }}
+      />
+
+      <FlatList
+        data={results}
+        keyExtractor={(item) => item.cca3}
+        renderItem={({ item }) => <Text>{item.name.common}</Text>}
+      />
+    </View>
+  );
 }
-
-export default Cat
